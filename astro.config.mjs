c9d2013c8109083c313b8b17eb110ef2ff51b019
gottigjavier <gottigjavier@gmail.com>
2024-08-29 20:28:06 -0300

    deno deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
+import deno from '@astrojs/deno';
 
-
-// https://astro.build/config
 export default defineConfig({
-
+  output: 'server',
+  adapter: deno(),
 });
